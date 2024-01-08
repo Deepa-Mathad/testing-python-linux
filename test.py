e4b3a3292f836735840010adc607f7ab7e446646
Deepa-Mathad <67700214+Deepa-Mathad@users.noreply.github.com>
2024-01-08 18:09:55 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+from hello import initial
+import argparse
+import os
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+# parser = argparse.ArgumentParser(
+#     description='Utility that runs Static Code Analysis and identifies the QWidgets classes for the Fusion360 project')
+# parser.add_argument('--f360', required=True, help='Location of Fusion360 project')
+# parser.add_argument('--scanner', required=True, help='Path to clang-static-analzer. Use default ~/clang-static-analyzer if not path is present.')
+
+# args = parser.parse_args()
+
+# global F360_PATH
+# F360_PATH = args.f360
+
+# global SCANNER_PATH
+# SCANNER_PATH = args.scanner
+# print(F360_PATH,"f360")
+initial()
+
+print("Deepa")
